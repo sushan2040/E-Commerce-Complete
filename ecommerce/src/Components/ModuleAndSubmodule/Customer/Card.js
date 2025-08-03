c4b5bc3a6a476db84db26823789286999bb96de0
sushant2040 <sushantnikam2040@gmail.com>
2025-08-03 15:03:17 +0530

    changed frontend code or better image width and height fit

--- ecommerce/src/Components/ModuleAndSubmodule/Customer/Card.js
+++ ecommerce/src/Components/ModuleAndSubmodule/Customer/Card.js
@@ -12,32 +12,46 @@ export default function Card({ productId, productName, productImages, productCos
         <div className="col-sm-3 col-md-2">
             <NavLink to={`/ecommerce/product/view?productId=${productId}`} className="card mt-3" >
 
-                {productImages &&
-                    <div className="">
-                        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active img-container" data-bs-interval="10000">
-                                    {productImages[0] && <img src={productImages[0].imagePath} class="d-block w-100" alt="..." />}
-                                </div>
-                                <div class="carousel-item img-container" data-bs-interval="2000">
-                                    {productImages[1] && <img src={productImages[1].imagePath} class="d-block w-100" alt="..." />}
-                                </div>
-                                <div class="carousel-item img-container">
-                                    {productImages[2] && <img src={productImages[2].imagePath} class="d-block w-100" alt="..." />}
-                                </div>
+                {productImages && (
+                    <div className="carousel-wrapper">
+                        <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+                            <div className="carousel-inner">
+                                <div
+                                    className="carousel-item active img-container"
+                                    data-bs-interval="10000"
+                                    style={{ backgroundImage: `url(${productImages[0]?.imagePath})` }}
+                                ></div>
+                                <div
+                                    className="carousel-item img-container"
+                                    data-bs-interval="2000"
+                                    style={{ backgroundImage: `url(${productImages[1]?.imagePath})` }}
+                                ></div>
+                                <div
+                                    className="carousel-item img-container"
+                                    style={{ backgroundImage: `url(${productImages[2]?.imagePath})` }}
+                                ></div>
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
+                            <button
+                                className="carousel-control-prev"
+                                type="button"
+                                data-bs-target="#carouselExampleInterval"
+                                data-bs-slide="prev"
+                            >
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
                             </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
+                            <button
+                                className="carousel-control-next"
+                                type="button"
+                                data-bs-target="#carouselExampleInterval"
+                                data-bs-slide="next"
+                            >
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
                             </button>
                         </div>
-
                     </div>
-                }
+                )}
                 <label className="form-label" style={{ fontSize: '0.8rem', width: '7rem' }}>{productName}</label>
                 <div className="row">
                     <span className="">{productCost}</span>
